da18bd8574e1bc7ed86335666d96ea771ec38253
Karol <kariurek@gmail.com>
2021-11-22 13:39:56 +0100

    nav menu update

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-var menuShown = 0;
-var list = document.getElementById('sideList');
+var list = document.getElementById("sideList");
 
 function showMenu() {
     list.classList.toggle("hide-list");
+}
+
+var artShown = 1;
+var hideBtn = document.getElementById('hideBtn')
+function hideArt() {
+  var unnecessary = document.getElementsByClassName("unc");
+  var i;
+  if (artShown=='1') {
+    for (i = 0; i < unnecessary.length; i++) {
+        unnecessary[i].style.display = "none";
+        artShown = 0;
+        hideBtn.innerHTML = "Show non essential articles"
+    }
+  } else {
+    for (i = 0; i < unnecessary.length; i++) {
+        unnecessary[i].style.removeProperty('display');
+        artShown = 1;
+        hideBtn.innerHTML = "Hide non essential articles"
+    }
+    }
 }
